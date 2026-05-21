054c8af2f1dc9870bc8cec4d08cb51cc2297cfa1
mindburnlabs <mindburnlabs@gmail.com>
2026-05-21 15:29:23 +0300

    Normalize design system component barrel exports

--- packages/design-system-core/src/components/index.ts
+++ packages/design-system-core/src/components/index.ts
@@ -18,8 +18,7 @@ export * from "./theme-provider";
 export * from "./announce";
 export * from "./telemetry";
 export * from "./workbench";
-export * from './canvas';
-export * from './diff';
-export * from './annotation';
-
+export * from "./canvas";
+export * from "./diff";
+export * from "./annotation";
 
